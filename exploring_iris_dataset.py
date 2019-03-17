4795b8d832cdae8d400bcb92accc3585bc32d949
Jennifer Ryan <G00376325@gmit.ie>
2019-03-17 00:41:24 +0000

    Playing around with machine learning tutorial

---
+++ exploring_iris_dataset.py
@@ -0,0 +1,31 @@
+# Machine Learning Project https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+# We can get a quick idea of how many instances (rows) and how many attributes (columns) the data contains with the shape property.
+print(dataset.shape)
+
+# peek at the data
+print(dataset.head(20))
+
+# Now we can take a look at a summary of each attribute.
+# This includes the count, mean, the min and max values as well as some percentiles.
+print(dataset.describe())
