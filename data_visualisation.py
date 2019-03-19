d0093a15b89f1d092a5602cfaf63cd435f795180
Jennifer Ryan <G00376325@gmit.ie>
2019-03-19 21:36:00 +0000

    Practicing graphs

---
+++ data_visualisation.py
@@ -0,0 +1,33 @@
+# Machine Learning Project https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
+# Graphs
+
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+
+# univariate plots - to better understand each attribute
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# multivariate plots - to better understand the relationships between attributes.
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+# Note the diagonal grouping of some pairs of attributes. This suggests a high correlation and a predictable relationship.
