a5e5bd2d549a5c23f2fd57e2a3e02b3f956f7ede
Jennifer Ryan <G00376325@gmit.ie>
2019-03-24 20:20:39 +0000

    changed name

---
+++ pandas_exp.py
@@ -0,0 +1,23 @@
+# experimenting with pandas https://pandas.pydata.org/pandas-docs/stable/getting_started/10min.html
+# https://data36.com/pandas-tutorial-1-basics-reading-data-files-dataframes-data-selection/
+
+import numpy as np
+import pandas as pd
+
+# load csv creating a pandas dataframe
+iris = pd.read_csv('iris.csv', delimiter = ',')
+
+# prints first 5 entries
+print(iris.head())
+
+# prints last 5 entries
+print(iris.tail())
+
+# prints 10 random entries
+print(iris.sample(10))
+
+# prints select particular columns
+print (iris[['sepal_length', 'species']])
+
+# prints rows filtered by type. this could be useful when trying to separate species for analysis 
+print(iris[iris.species == 'setosa'])
