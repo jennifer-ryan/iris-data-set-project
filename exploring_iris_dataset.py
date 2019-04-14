5a748a4ac652738d19e8b9b1085a8b2542b65a78
Jennifer Ryan <G00376325@gmit.ie>
2019-04-14 22:42:15 +0100

    figured out how to split the dataset

--- exploring_iris_dataset.py
+++ exploring_iris_dataset.py
@@ -1,41 +1,51 @@
 # Machine Learning Project https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
 # Data Summary
 
+#Go back to this:
+# guide to exploring datasets https://www.shanelynn.ie/using-pandas-dataframe-creating-editing-viewing-data-in-python/
+# selecting subsets https://medium.com/dunder-data/selecting-subsets-of-data-in-pandas-6fcd0170be9c
+# exploration https://medium.com/@harimittapalli/exploratory-data-analysis-iris-dataset-9920ea439a3e
+
+# Steps in data exploration 
+# https://datahero.com/blog/2013/11/20/5-beginners-steps-to-investigating-your-dataset/
+# https://www.quora.com/What-are-the-steps-include-in-data-exploration
+
+
 # Load libraries
-import pandas
-from pandas.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
+import pandas as pd
+
 
 # Load dataset
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv(url, names=names)
-print( )
+iris = pd.read_csv('iris.csv', delimiter = ',')
 
 # We can get a quick idea of how many instances (rows) and how many attributes (columns) the data contains with the shape property.
-print(dataset.shape)
-print( )
+print(iris.shape)
+
 
 # peek at the data
-print(dataset.head(20))
-print( )
+print(iris.head(20))
 
 # Now we can take a look at a summary of each attribute.
 # This includes the count, mean, the min and max values as well as some percentiles.
-print(dataset.describe())
-print( )
+print(iris.describe())
+
 
 # class distribution
 # the number of instances (rows) that belong to each class
-print(dataset.groupby('class').size())
-print( )
+print(iris.groupby('species').size())
+
+
+# data types
+iris.dtypes
+
+# splitting the dataset by species
+# help from https://medium.com/dunder-data/selecting-subsets-of-data-in-pandas-6fcd0170be9c
+
+index = iris.index
+columns = iris.columns
+values = iris.values
+
+setosa = iris.loc[0:49]
+versicolor = iris.loc[50:99]
+virginica = iris.loc[100:149]
+
