e47e44de6dc9ced390d16d40a1da8cdced44888a
Jennifer Ryan <G00376325@gmit.ie>
2019-04-14 22:44:38 +0100

    swarmplots

--- data_visualisation.py
+++ data_visualisation.py
@@ -27,15 +27,23 @@ plt.show()
 # **SEPAL AND PETAL SCATTERPLOTS SHOW SETOSA SEPARATION**
 #https://github.com/venky14/Machine-Learning-with-Iris-Dataset/blob/master/Iris%20Species%20Dataset%20Visualization.ipynb
 # Nice scatterplot of sepal length/width. Saved as scatter.sepal
-#import seaborn as sns
+import seaborn as sns
 #sns.FacetGrid(iris, hue = 'species', height=5).map(plt.scatter, 'sepal_length', 'sepal_width').add_legend()
 # same below but with petals
 #sns.FacetGrid(iris, hue = 'species', height=5).map(plt.scatter, 'petal_length', 'petal_width').add_legend()
 #plt.show()
 
-
+# Swarmplots
+# https://seaborn.pydata.org/generated/seaborn.swarmplot.html
+sns.swarmplot(x = 'species', y = 'sepal_length', data = iris)
+sns.swarmplot(x = 'species', y = 'sepal_width', data = iris)
+sns.swarmplot(x = 'species', y = 'petal_length', data = iris)
+sns.swarmplot(x = 'species', y = 'petal_width', data = iris)
+plt.show()
 
 # Pivot Table
 # appended from https://www.kaggle.com/lalitharajesh/iris-dataset-exploratory-data-analysis
 #pd.pivot_table(iris, index=['species'], values = ['sepal_length'], aggfunc=[np.mean, np.std])
+
+
 
