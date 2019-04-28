1604cc2e0d5f5775d94a7a19af5c13cb41c16b62
Jennifer Ryan <G00376325@gmit.ie>
2019-04-28 20:12:38 +0100

    Code complete

--- test_parameters.py
+++ test_parameters.py
@@ -18,13 +18,14 @@ y = iris.target
 k = 3
 
 # Checks from 3 - 23 to assess best fit K value for the model. 
+# Iterates through KNN model creation to find best fit.
 while k < 23:
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2) # 0.2 value changed to 0.3 to test 30% test size also
     knn = knc(n_neighbors = k)
     knn.fit(x_train, y_train)
     test_prediction = knn.predict(x_test)
     accuracy = metrics.accuracy_score(y_test, test_prediction)
-    print(accuracy)
+    print(k, "=", accuracy)
     k += 2
 # Code run through 10 times with test_size set to 0.2 and a further 10 times with test_size set to 0.3
 # Results saved to tables. See Images/k_values.PNG 
